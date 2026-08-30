1ad5b1c19a571c6af045ff15af130aa864f8d627
Tyler Daw <tylerdaw87@outlook.com>
2026-08-30 00:13:26 -0500

    Added vault module

---
+++ src/vaultManagement/vaultDataStructure.ts
@@ -0,0 +1,34 @@
+interface VaultEntry {
+    site: string;
+    username: string;
+    password: string;
+}
+type Vault = VaultEntry[];
+
+export function addEntry(vault: Vault, site: string, username: string, password: string): Vault {
+    const newEntry: VaultEntry = { site, username, password };
+    return [...vault, newEntry];
+}
+
+export function getEntriesForSite(vault: Vault, site: string): VaultEntry[] {
+    return vault.filter((entry) => entry.site === site);
+}
+
+export function removeEntry(vault: Vault, site: string, username: string): Vault {
+    return vault.filter((entry) => entry.site !== site || entry.username !== username);
+}
+
+export function serializeVault(vault: Vault): string {
+    return JSON.stringify(vault);
+}
+
+export function deserializeVault(serializedVault: string): Vault{
+    try {
+        const parsedVault = JSON.parse(serializedVault) as Vault;
+        return parsedVault;
+    }
+    catch (error) {
+        console.error("Failed to deserialize vault:", error);
+        throw new Error("Failed to deserialize vault: Invalid JSON format");
+    }
+}
